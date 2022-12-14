24f9a17c67711073be2c34b722d655fe90f1f105
Trongar <sanchesedgar97@gmail.com>
2022-12-14 16:31:50 -0600

    Fix gitignore

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head({ params }: { params: { slug: string } }) {
+    return (
+      <>
+        <title>DinoCore: Next.Js Starter Template</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon"/>  
+      </>
+    );
+  }
